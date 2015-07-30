ae036cc7bbc84acca4111dee27da79bf980c6a0e
robosoft <robosoft@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-30 18:44:23 +0000

    = 1.1.0 = *   Added new clone gallery settings function *   Fast inheriting of the gallery settings from another gallery *   Define gallery settings in one place and create as much galleries with the same interface as you need. Just create new gallery, upload images and inherit settings from another created gallery. Very useful tools for websites with a lot of galleries *   Life and time saver for webmasters
    git-svn-id: https://plugins.svn.wordpress.org/robo-gallery/tags/1.0.2@1209890 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ cmb2/fields/toolbox/cmb-field-toolbox.php
@@ -0,0 +1,86 @@
+<?php
+/*
+*      Robo Gallery     
+*      Version: 1.0
+*      By Robosoft
+*
+*      Contact: http://robosoft.co
+*      Created: 2015
+*      Licensed under the GPLv2 license - http://opensource.org/licenses/gpl-2.0.php
+*
+*      Copyright (c) 2014-2015, Robosoft. All rights reserved.
+*      Available only in http://robosoft.co/
+*/
+
+
+class RBS_TOOLBOX {
+
+	public function hooks() {
+		add_action( 'cmb2_render_toolbox', array( $this, 'rbs_cmb2_render_toolbox_field_callback' ), 10, 5 );
+		add_action( 'admin_enqueue_scripts', array( $this, 'rbs_setup_admin_scripts' ) );
+		if(!ROBO_GALLERY_PRO) add_action( 'in_admin_header', array( $this, 'rbs_setup_admin_header' ) );
+	}
+
+	public function rbs_cmb2_render_toolbox_field_callback(){}
+
+	public function render_color_picker( $field, $field_escaped_value, $field_object_id, $field_object_type, $field_type_object ) {}
+	
+	public function rbs_setup_admin_header(){
+		echo '<div id="rbs_showInformation" '
+						.'style="display: none;" '
+						.'data-body="rbs_edit" '
+						.'data-open="0" '
+						.'data-title="'.__('Get Robo Gallery Pro version').'" '
+						.'data-close="'.__('Close', 'rbs_gallery').'" '
+						.'data-info="'.__('Get Pro version').'"'
+					.'>'
+					.__('This function available in PRO version', 'rbs_gallery')
+				.'</div>';
+	}
+
+	public function rbs_setup_admin_scripts() {
+
+		wp_enqueue_style("wp-jquery-ui-dialog");
+		wp_enqueue_script('jquery-ui-dialog');
+
+		if(!ROBO_GALLERY_PRO){
+			wp_enqueue_script('robo-gallery-info', ROBO_GALLERY_URL.'js/admin/info.js', array( 'jquery' ), ROBO_GALLERY_VERSION, true ); 
+			wp_enqueue_style ('robo-gallery-info', ROBO_GALLERY_URL.'css/admin/info.css', array( ), ROBO_GALLERY_VERSION );
+		}
+
+		//admin.base
+		wp_enqueue_script( 	'rbs-toolbox-admin-edit', ROBO_GALLERY_URL.'js/admin/edit.js', 	array( 'jquery' ), ROBO_GALLERY_VERSION, true );
+		wp_enqueue_style ( 	'rbs-toolbox-admin-edit',  ROBO_GALLERY_URL.'css/admin/edit.css' );
+		
+		//bootstrap
+		wp_enqueue_script( 	'rbs_bootstrap', 			ROBO_GALLERY_URL.'addons/bootstrap/js/bootstrap.min.js', 		array('jquery'), ROBO_GALLERY_VERSION, true);
+		wp_enqueue_style ( 	'rbs_bootstrap', 			ROBO_GALLERY_URL.'addons/bootstrap/css/bootstrap.css',			array(), ROBO_GALLERY_VERSION, 'all');
+		//wp_enqueue_style ( 'rbs_bootstrap_theme', 	ROBO_GALLERY_URL.'addons/bootstrap/bootstrap-theme.css' );
+
+		//checkbox
+		wp_enqueue_script( 'rbs-toolbox-toggles-js', 	ROBO_GALLERY_URL.'addons/toggles/js/bootstrap-toggle.min.js', 	array( 'jquery' ), ROBO_GALLERY_VERSION, true );
+		wp_enqueue_style(  'rbs-toolbox-toggles-css',	ROBO_GALLERY_URL.'addons/toggles/css/bootstrap-toggle.css', 	array(), ROBO_GALLERY_VERSION, 'all' );
+		
+		//iconPicker
+		wp_enqueue_script( 	'rbs-toolbox-iconset', 		ROBO_GALLERY_URL.'addons/bootstrap-iconpicker/js/iconset/iconset-fontawesome-4.1.0.min.js', 		array( 'jquery' ), ROBO_GALLERY_VERSION, true );
+		wp_enqueue_script( 	'rbs-toolbox-icon', 		ROBO_GALLERY_URL.'addons/bootstrap-iconpicker/js/bootstrap-iconpicker.js', 							array( 'jquery', 'rbs-toolbox-iconset' ), ROBO_GALLERY_VERSION, true );		
+		wp_enqueue_style( 	'rbs-toolbox-icon-css', 	ROBO_GALLERY_URL.'addons/bootstrap-iconpicker/css/bootstrap-iconpicker.css', 						array(), ROBO_GALLERY_VERSION, 'all' );
+		wp_enqueue_style( 	'rbs-toolbox-icon-fonts', 	ROBO_GALLERY_URL.'addons/bootstrap-iconpicker/icon-fonts/font-awesome-4.1.0/css/font-awesome.css', 	array(), ROBO_GALLERY_VERSION, 'all' );
+		
+		//color
+		wp_enqueue_script( 'rbs-toolbox-color-tinycolor', ROBO_GALLERY_URL.'addons/color/bootstrap.colorpickersliders.tinycolor.js', 	array( 'jquery' ), ROBO_GALLERY_VERSION, true );
+		wp_enqueue_script( 'rbs-toolbox-color', ROBO_GALLERY_URL.'addons/color/bootstrap.colorpickersliders.js', 						array( 'jquery' ), ROBO_GALLERY_VERSION, true );
+		wp_enqueue_style( 'rbs-toolbox-color', ROBO_GALLERY_URL.'addons/color/bootstrap.colorpickersliders.css', 						array(), ROBO_GALLERY_VERSION, 'all' );
+
+		//slider
+		wp_enqueue_script( 'rbs-toolbox-bootstrap-slider', ROBO_GALLERY_URL.'addons/bootstrap-slider/js/bootstrap-slider.js', 	array( 'jquery' ), ROBO_GALLERY_VERSION, true );
+		wp_enqueue_style( 'rbs-toolbox-bootstrap-slider', ROBO_GALLERY_URL.'addons/bootstrap-slider/css/bootstrap-slider.css', 	array(), ROBO_GALLERY_VERSION, 'all' );
+		
+		//select
+		wp_enqueue_script('rbs-toolbox-bootstrap-select', ROBO_GALLERY_URL.'addons/bootstrap-select/js/bootstrap-select.js', 	array( 'jquery' ), ROBO_GALLERY_VERSION, true );
+		wp_enqueue_style( 'rbs-toolbox-bootstrap-select', ROBO_GALLERY_URL.'addons/bootstrap-select/css/bootstrap-select.css', 	array(), ROBO_GALLERY_VERSION, 'all' );
+	
+	}
+}
+$rbs_tololbox = new RBS_TOOLBOX();
+$rbs_tololbox->hooks();
