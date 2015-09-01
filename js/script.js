c8d2afa88e349fd3ac1b88689954718e7292ad60
robosoft <robosoft@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-09-01 13:09:55 +0000

    = 1.2.4 = *   Added Dutch language
    git-svn-id: https://plugins.svn.wordpress.org/robo-gallery/tags/1.2.3@1235414 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/script.js
@@ -0,0 +1,27 @@
+/*
+*      Robo Gallery     
+*      Version: 1.0
+*      By Robosoft
+*
+*      Contact: http://robosoft.co
+*      Created: 2015
+*      Licensed under the GPLv2 license - http://opensource.org/licenses/gpl-2.0.php
+*
+*      Copyright (c) 2014-2015, Robosoft. All rights reserved.
+*      Available only in http://robosoft.co/
+*/
+
+(function($) {
+	jQuery('.robo_gallery').each( function(){
+		var objectOptions = window[ jQuery(this).data('options') ];
+		//console.log(objectOptions);
+		var realOptions = jQuery.extend({},objectOptions);
+		var gallery = jQuery(this).collagePlus( realOptions ); 
+		if( roboGalleryDelay != undefined && roboGalleryDelay > 0 ){
+			setTimeout(function () {
+				gallery.eveMB('resize');
+				console.log("roboGallery - resize"); 
+			}, roboGalleryDelay);
+		}
+	});
+})(jQuery);
