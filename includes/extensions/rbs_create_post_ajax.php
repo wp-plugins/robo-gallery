9582c8100800eaf53d3d143a516414b632546884
robosoft <robosoft@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-11-19 12:50:26 +0000

    = 1.5.3 = *   New option for post generator *   Added preview button in post dialog wizard
    git-svn-id: https://plugins.svn.wordpress.org/robo-gallery/tags/1.5.2@1289788 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/extensions/rbs_create_post_ajax.php
@@ -0,0 +1,118 @@
+<?php
+if ( ! defined( 'WPINC' ) )  die;
+
+rbs_gallery_include('class.postcontroller.php', ROBO_GALLERY_EXTENSIONS_PATH);
+
+
+if(!function_exists('rbs_ajax_create_article')){
+	function rbs_ajax_create_article(){
+		if( 
+				isset($_POST['galleryid']) && (int)$_POST['galleryid'] && 
+				isset($_POST['categoryid']) && (int)$_POST['categoryid'] 
+		){
+			$galleryid = intval( $_POST['galleryid'] );
+			$categoryid = intval( $_POST['categoryid'] );
+
+			$post_info = get_post( $galleryid );
+			
+			if( gettype($post_info)!='object' ) {
+				echo '<p><strong>'.__('Post not created. Error: ','rbs_gallery').'</strong><br><p>empty gallery id</p>';
+				die();
+			}
+	
+			$Poster = new PostController;
+			
+			$title = $post_info->post_title;
+			if(isset( $_POST['articletitle'] ) && $_POST['articletitle'] ) $title = wp_kses_data($_POST['articletitle']);
+			
+			$slug = 'post_'.$post_info->post_name;
+			if(isset( $_POST['articleslug'] ) && $_POST['articleslug'] ) $slug = wp_kses_data($_POST['articleslug']);
+			
+
+
+			$Poster->set_title( $title );
+			$Poster->add_category( array($categoryid) );
+			$Poster->set_type( "post" );
+			$Poster->set_content( '[robo-gallery id="'.$galleryid.'"]' );
+			$Poster->set_author_id( get_current_user_id() );
+			$Poster->set_post_slug( $slug );
+			$Poster->set_post_state( "publish" );
+			$Poster->create();
+
+			if( isset($Poster->errors) && count($Poster->errors) ){
+				echo '<p><strong>'.__('Post not created. Error: ','rbs_gallery').'</strong><br>';
+				for ($i=0; $i < count($Poster->errors); $i++) { 
+					$error = $Poster->errors[$i];
+					echo ' &nbsp;&nbsp; - '.$error.'<br>';
+				}
+				echo '</p>';
+			} else {
+				echo '<p>'.__('Post created','rbs_gallery').'</p>';  
+			}
+		} else {
+			echo '<p><strong>'.__('Error: input value','rbs_gallery').'</strong></p>';
+		}
+		die();
+	}
+}
+
+if(!function_exists('rbs_ajax_create_article_form')){
+	function rbs_ajax_create_article_form(){ 
+		$args = array(
+			'show_option_all'    => '',
+			'show_option_none'   => '',
+			'option_none_value'  => '-1',
+			'orderby'            => 'ID', 
+			'order'              => 'ASC',
+			'show_count'         => 0,
+			'hide_empty'         => 0, 
+			'child_of'           => 0,
+			'exclude'            => '',
+			'echo'               => 1,
+			'selected'           => 0,
+			'hierarchical'       => 1, 
+			'name'               => 'cat',
+			'id'                 => 'rbs_post_create_category',
+			'class'              => 'form-control',
+			'depth'              => 0,
+			'tab_index'          => 0,
+			'taxonomy'           => 'category',
+			'hide_if_empty'      => false,
+			'value_field'	     => 'term_id',	
+		);
+		if( !isset($_POST['galleryid']) || !(int)$_POST['galleryid'] ){
+			echo '<p><strong>'.__('Post not created. Error: ','rbs_gallery').'</strong><br><p>'._e('Empty  gallery ID','rbs_gallery').'</p>';
+			return ;
+		} ; 
+		
+		$post_info = get_post( (int) $_POST['galleryid'] );
+
+		if( gettype($post_info)!='object' ) {
+			echo '<p><strong>'.__('Post not created. Error: ','rbs_gallery').'</strong><br><p>'._e('Incorrect  gallery ID','rbs_gallery').'</p>';
+			return ;
+		}
+
+		?>
+		<table class="form-table">
+			<tbody>
+				<tr>
+					<th scope="row"><label for="rbs_post_create_title"><?php _e('Select category','rbs_gallery'); ?></label></th>
+					<td><?php wp_dropdown_categories( $args ); ?></td>
+				</tr>
+
+				<tr>
+					<th scope="row"><label for="rbs_post_create_title">Post Title</label></th>
+					<td><input name="rbs_post_create_title" id="rbs_post_create_title" value="<?php echo $post_info->post_title ;?>" class="regular-text" type="text"></td>
+				</tr>
+
+				<tr>
+					<th scope="row"><label for="rbs_post_create_slug">Post Slug</label></th>
+					<td><input name="rbs_post_create_slug" id="rbs_post_create_slug" value="<?php echo 'post_'.$post_info->post_name;?>" class="regular-text" type="text"></td>
+				</tr>
+			</tbody>
+		</table>
+		<?php
+		
+		echo '<p>'.__('Short tag of the gallery will be insert into created article.','rbs_gallery').'</p>';
+	}
+}
---
+++ includes/options/rbs_gallery_options_tools.php
@@ -0,0 +1,39 @@
+<?php 
+/*
+*      Robo Gallery     
+*      Version: 1.5
+*      By Robosoft
+*
+*      Contact: http://robosoft.co
+*      Created: 2015
+*      Licensed under the GPLv2 license - http://opensource.org/licenses/gpl-2.0.php
+*
+*      Copyright (c) 2014-2015, Robosoft. All rights reserved.
+*      Available only in http://robosoft.co/
+*/
+
+
+$tools_group = new_cmb2_box( array(
+    'id'            => ROBO_GALLERY_PREFIX.'tools_metabox',
+    'title'         => '<span class="dashicons dashicons-admin-tools"></span> '.__('Gallery Tools','rbs_gallery'),
+    'object_types'  => array( ROBO_GALLERY_TYPE_POST ),
+    'context'       => 'side',
+    'priority'      => 'low',
+  //  'closed'        => rbs_gallery_set_checkbox_default_for_new_post(0),
+));
+
+if(isset($_GET['post'])){
+	$tools_group->add_field( array(
+	    'id'            => ROBO_GALLERY_PREFIX.'create_acticle',
+	    'type'          => 'title',
+	    'before_row'    => '<div class="rbs_block">'
+	    	.'<div class="rbs-center-block rbs-margin-block">'
+		    	.'<button id="rbs_create_article" data-galleryid="'.(int)$_GET['post'].'" class="btn btn-info btn-lg ">'
+		    		.'<span class="glyphicon glyphicon-plus" aria-hidden="true"></span> '
+		    		.__('Create Post','rbs_gallery')
+		    	.'</button>'
+		    	.'<p class="rbs_desc">'.__('Here you can create and customize new post with gallery inside it','rbs_gallery').'</p> '
+		    .'</div>',
+	    'after_row'    	=> '</div>',
+	));
+}
